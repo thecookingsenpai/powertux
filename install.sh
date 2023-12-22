c657ad4591a74fda12535fe3fd9fdb00c554493a
thecookingsenpai <elmastrococinero@gmail.com>
2023-12-22 19:09:54 +0100

    Trunked

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 THIS=$(pwd)
-SHELLTYPE=$(echo $SHELL | awk -F"/" '{print $4}')
-echo $SHELLTYPE
-SHELLRC="."${SHELLTYPE}"rc"
-echo "$SHELLRC"
+SHELLTYPE=$(echo "${SHELL}" | awk -F"/" '{print $4}')
+echo "${SHELLTYPE}"
+SHELLRC='.'${SHELLTYPE}'rc'
+echo "${SHELLRC}"
 
 if [[ ! -f ${HOME}/"${SHELLRC}" ]]; then
 	echo "No suitable configuration file found for ${SHELLTYPE} (tested ${SHELLRC})"
@@ -19,8 +19,8 @@ no) echo "Ok" ;;
 esac
 
 cp -R powertux_folder "${HOME}"/
-echo "PATH=${PATH}:${HOME}/powertux_folder/bin" >>"${HOME}"/"$SHELLRC"
-echo "sh ${HOME}/powertux_folder/powertux.sh" >>"$HOME"/"${SHELLRC}"
-cd $THIS
-echo "Done! Add your binaries into $HOME/powertux_folder/bin"
-echo "Please restart your shell or source your $SHELLRC to apply the settings in this session"
+echo "PATH=${PATH}:${HOME}/powertux_folder/bin" >>"${HOME}"/"${SHELLRC}"
+echo "sh ${HOME}/powertux_folder/powertux.sh" >>"${HOME}"/"${SHELLRC}"
+cd "${THIS}" || exit
+echo "Done! Add your binaries into ${HOME}/powertux_folder/bin"
+echo "Please restart your shell or source your ${SHELLRC} to apply the settings in this session"
