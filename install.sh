2df10d2e2b2e44cda6706acf39098d93e9353484
thecookingsenpai <elmastrococinero@gmail.com>
2023-12-22 19:06:33 +0100

    System wide installation supportù

--- install.sh
+++ install.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-THIS=$(PWD)
-mv custom $HOME/
-mv custom.sh $HOME/custom.sh
-echo "source $HOME/custom.sh" >> $HOME/.profile
-cd $HOME
-source .profile
+THIS=$(pwd)
+SHELLTYPE=$(echo $SHELL | awk -F"/" '{print $4}')
+echo $SHELLTYPE
+SHELLRC="."$SHELLTYPE"rc"
+echo $SHELLRC
+
+if [ ! -f $HOME/$SHELLRC ]; then
+	echo "No suitable configuration file found for $SHELLTYPE (tested $SHELLRC)"
+	exit 0
+fi
+
+echo "Configuration is $HOME/$SHELLRC"
+
+read -p "Do you want to install the binaries system wide (in /usr/bin)? This requires root (yes/no)" yn
+case $yn in
+	yes ) sudo cp powertux_folder/bin/* /usr/bin/;;
+	no ) echo "Ok";;
+esac
+
+cp -R powertux_folder $HOME/
+echo "PATH=$PATH:$HOME/powertux_folder/bin" >> $HOME/$SHELLRC
+echo "sh $HOME/powertux_folder/powertux.sh" >> $HOME/$SHELLRC
 cd $THIS
-echo "Done! Add your binaries into $HOME/custom/bin or $HOME/custom/local/bin"
+echo "Done! Add your binaries into $HOME/powertux_folder/bin"
+echo "Please restart your shell or source your $SHELLRC to apply the settings in this session"
--- powertux_folder/powertux.sh
+++ powertux_folder/powertux.sh
@@ -1,5 +1,6 @@
 #screen -d -m autokey-qt # So that it does not spawn any window
 #screen -d -m conky # and customizations if needed
+
 echo "TCSenpai's Custom Script"
 echo "Active customizations:"
 echo "> gitup; add, commit, lint, check, autocorrect, recommit and push all in one"
